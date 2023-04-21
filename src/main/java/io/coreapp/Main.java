cc15647982019280b7ba68461e89b397bc44deab
Andrey Kretsev <akrecev@gmail.com>
2023-04-21 23:59:10 +0300

    feat add sorting

--- src/main/java/io/coreapp/Main.java
+++ src/main/java/io/coreapp/Main.java
@@ -1,8 +1,10 @@
 package io.coreapp;
 
 import io.coreapp.manager.CitiesManager;
+import io.coreapp.model.City;
 
 import java.io.File;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,8 +12,19 @@ public class Main {
         File file = new File("data/cities.csv");
 
         CitiesManager citiesManager = new CitiesManager();
+        List<City> cityList = citiesManager.loadCities(file);
 
-        citiesManager.loadCities(file);
+        // Сортировка по наименованию города
+        List<City> sortedListByName = citiesManager.sortByName(cityList);
+        citiesManager.printCityList(sortedListByName);
+
+        System.out.println("\n");
+        System.out.println("_________________________________________________________________________________________");
+        System.out.println("\n");
+
+        // Сортировка по федеральному округу и наименованию города
+        List<City> sortedListByDistrictAndName = citiesManager.sortByDistrictAndName(cityList);
+        citiesManager.printCityList(sortedListByDistrictAndName);
 
     }
 }
--- src/main/java/io/coreapp/manager/CitiesManager.java
+++ src/main/java/io/coreapp/manager/CitiesManager.java
@@ -5,17 +5,20 @@ import io.coreapp.model.City;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Scanner;
 
 public class CitiesManager {
 
     final static String SEPARATOR = ";";
 
-    public void loadCities(File file) {
+    public List<City> loadCities(File file) {
+        List<City> cityList = new ArrayList<>();
 
         try {
             Scanner scanner = new Scanner(file);
-            System.out.println();
 
             while (scanner.hasNext()) {
                 String line = scanner.nextLine();
@@ -28,13 +31,31 @@ public class CitiesManager {
                         data.length < 5 ? 0 : Integer.parseInt(data[4].trim()),       // population
                         data.length < 6 ? "" : data[5].trim()                         // foundation
                 );
-                System.out.println(city + "\n");
+                cityList.add(city);
             }
 
         } catch (IOException e) {
             throw new FileReadException("File reading error");
         }
 
+        return cityList;
+    }
+
+    public List<City> sortByName(List<City> cityList) {
+        cityList.sort(Comparator.comparing(City::getName));
+        return  cityList;
+    }
+
+    public List<City> sortByDistrictAndName(List<City> cityList) {
+        cityList.sort(Comparator.comparing(City::getDistrict).thenComparing(City::getName));
+        return  cityList;
+    }
+
+    public void printCityList(List<City> cityList) {
+        for (City city : cityList) {
+            System.out.println();
+            System.out.println(city);
+        }
     }
 
 }
